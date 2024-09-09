f8c9357d0114cfa049b6641e57d4df4f41ebb249
GlanzWen <281616236@qq.com>
2024-09-09 17:31:20 +0800

    init push

---
+++ src/test/java/com/example/aroundpoint/AroundpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aroundpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
